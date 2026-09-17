1fbb35dfbbca0af7f476e99d737a959d8b18bfec
Dno-0749 <hainc0749@ut.edu.vn>
2026-09-17 12:54:53 +0700

    Revert "Merge be into test"
    This reverts commit a7d47d57bc2c607ca6dcb8fa228de449f6a844cd, reversing
    changes made to 7c39d85e7e5d9ed2c545dc41a3d61a588480fb69.

--- api/schemas/report.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class InvoiceResponseSchema(Schema):
-    id = fields.Int()
-    onboard_account_id = fields.Int()
-    total_amount = fields.Decimal(as_string=True)
-    status = fields.Str()
-    issued_at = fields.Raw()
